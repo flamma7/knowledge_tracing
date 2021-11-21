f7df3039933ee5958dcbdde721dc4b1060e581fc
Luke Barbier <lukebarbier12@gmail.com>
2021-11-20 23:49:35 -0800

    adding l3's data

--- learn.py
+++ learn.py
@@ -20,6 +20,7 @@ import pickle
 import numpy as np
 import os.path
 import time
+import datetime
 import os
 
 def get_selection_probs(arr):
@@ -30,6 +31,11 @@ class WorkingMemory:
         self.probs = probs # Probability I know each index
         self.indices = indices # index of words in working memory
         self.ledger = ledger # user responses
+
+    def add_ledger_block(self, question_ind, question, question_start_time, time_taken, response, correct):
+        assert type(correct) == bool
+        new_block = [len(self.ledger), question_ind, question, question_start_time, time_taken, response, correct]
+        self.ledger.append( new_block )
 
     def add_word(self, new_ind):
         if new_ind not in self.indices:
@@ -95,7 +101,7 @@ while True:
     if sum(sels) > 0: # 
         ind = int( work_mem.indices[ np.random.choice(len(work_mem.indices), 1, p=sel_probs) ] )
     else: # Pick a new word
-        print("Adding new word(s)!")
+        # print("Adding new word(s)!")
         all_indices = np.arange(NUM_WORDS)
         wm_indices = work_mem.indices
         unknown_words = all_indices[np.in1d(all_indices,wm_indices,invert=True)] # Get the questions not yet explored
@@ -107,9 +113,6 @@ while True:
             words_to_add = len(unknown_words)
         inds = np.random.choice(unknown_words, words_to_add,p=sel_probs, replace=False).astype(int) # select new wordss
 
-        # TODO Check that the above adding of multiple new words works ################# LEEFT OFF
-
-        print(f"Adding: {inds}")
         # Add all the words
         for ind in inds:
             work_mem.add_word(ind)
@@ -117,7 +120,10 @@ while True:
 
     french_word = french_words[ ind ] 
     eng_word = english_words[ ind ]
+    start_time = time.time()
+    start_time_dur = datetime.datetime.now()
     resp = input(f"Word: {french_word}\n")
+    dur = (datetime.datetime.now() - start_time_dur).total_seconds()
 
     pG = work_mem.get_guess_probability()
     prior = work_mem.probs[ind]
@@ -127,17 +133,22 @@ while True:
         break
     elif resp == eng_word:
         print("CORRECT!")
+        correct = True
         likelihood = (prior * (1 - pS)) / ( prior*(1-pS) + (1 - prior)*pG )
     else:
         print(f"Nope! {french_word} : {eng_word}")
+        correct = False
         likelihood = (prior * pS) / ( prior*pS + (1 - prior)*(1-pG) )
+    work_mem.add_ledger_block(ind, french_word, start_time, dur, resp, correct)
     post = likelihood + (1-likelihood)*pT
-    print(f"{prior} -> {post}")
+    # print(f"{prior} -> {post}")
     work_mem.update_prob(ind, post)
     print(work_mem.indices_probs)
     time.sleep(1)
     print("\n"*10)
 
-# with open(FILENAME, "wb") as output_file:
-#     pickle.dump(work_mem, output_file)
-#     print("Working Memory Saved")
+print(work_mem.ledger)
+
+with open(FILENAME, "wb") as output_file:
+    pickle.dump(work_mem, output_file)
+    print("Working Memory Saved")
